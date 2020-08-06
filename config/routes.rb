0e812da3ee26103fd368de2166f27746247cc7a4
Kurnia Muhamad <omenkzz@gmail.com>
2020-08-06 14:07:16 +0700

    Create upload photo feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: "posts#index"
   resources :posts
+  resources :photos, only: [:new, :create, :index, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

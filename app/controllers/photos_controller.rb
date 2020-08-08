4e7895e17e30e0f7c0e39a367bdb00c1b0cb6c15
Kurnia Muhamad <omenkzz@gmail.com>
2020-08-08 21:48:26 +0700

    Add edit photo feature

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -6,6 +6,20 @@ class PhotosController < ApplicationController
   def new
     @photo = Photo.new
   end
+
+  def edit
+    @photo = Photo.find(params[:id])
+  end
+
+  def update
+    @photo = Photo.find(params[:id])
+    if @photo.update_attributes(photo_params)
+      flash[:success] = 'Photo edited!'
+      redirect_to photos_path
+    else
+      render 'edit'
+    end
+  end
  
   def create
     @photo = Photo.new(photo_params)

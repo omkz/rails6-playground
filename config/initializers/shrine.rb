5cd7854e7e52735a152edf59e8131a6a5883f242
Kurnia Muhamad <omenkzz@gmail.com>
2020-08-15 23:36:40 +0700

    Using rails logger for shrine

--- config/initializers/shrine.rb
+++ config/initializers/shrine.rb
@@ -6,6 +6,8 @@ Shrine.storages = {
   store: Shrine::Storage::FileSystem.new("public", prefix: "uploads"),       # permanent
 }
 
+Shrine.logger = Rails.logger
+
 Shrine.plugin :activerecord           # loads Active Record integration
 Shrine.plugin :cached_attachment_data # enables retaining cached file across form redisplays
 Shrine.plugin :restore_cached_data    # extracts metadata for assigned cached files
